ba462c1ad1dd033eaa6c7faa8cec4880aefbecd3
Piotr Zawadzki <Piotr.Zawadzki@stepstone.com>
2017-03-09 11:11:50 +0100

    Extended progress bar animation time to 200 milliseconds

--- material-stepper/src/main/java/com/stepstone/stepper/internal/widget/ColorableProgressBar.java
+++ material-stepper/src/main/java/com/stepstone/stepper/internal/widget/ColorableProgressBar.java
@@ -21,7 +21,6 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
-import android.os.Build;
 import android.support.annotation.ColorInt;
 import android.support.annotation.RestrictTo;
 import android.support.v4.content.ContextCompat;
@@ -44,20 +43,17 @@ public class ColorableProgressBar extends ProgressBar {
 
     /**
      * Interpolator used for smooth progress animations.
-     * This is copied from {@link ProgressBar} on Android Nougat.
      */
-    private static final DecelerateInterpolator PROGRESS_ANIM_INTERPOLATOR =
-            new DecelerateInterpolator();
+    private static final DecelerateInterpolator PROGRESS_ANIM_INTERPOLATOR = new DecelerateInterpolator();
 
     /**
      * Duration of smooth progress animations.
-     * This is copied from {@link ProgressBar} on Android Nougat.
      */
-    private static final int PROGRESS_ANIM_DURATION = 80;
+    private static final int PROGRESS_ANIM_DURATION = 200;
 
     /**
      * A multiplier to be used when setting the current progress of this progress bar.
-     * It is needed to animate the progress changes below Android Nougat.
+     * It is needed to animate the progress changes.
      */
     private static final int PROGRESS_RANGE_MULTIPLIER = 100;
 
@@ -123,9 +119,7 @@ public class ColorableProgressBar extends ProgressBar {
     }
 
     public void setProgressCompat(int progress, boolean animate) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            setProgress(progress * PROGRESS_RANGE_MULTIPLIER, animate);
-        } else if (animate) {
+        if (animate) {
             final ObjectAnimator animator = ObjectAnimator.ofInt(this, PROGRESS_PROPERTY, getProgress(), progress * PROGRESS_RANGE_MULTIPLIER);
             animator.setDuration(PROGRESS_ANIM_DURATION);
             animator.setInterpolator(PROGRESS_ANIM_INTERPOLATOR);
